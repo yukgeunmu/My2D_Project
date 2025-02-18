8766cedaa5684141000260f412d0b72339be8444
yukgeunmu <36117247+yukgeunmu@users.noreply.github.com>
2025-02-18 09:41:03 +0900

    [feat] 플래피버드 점프 움직임 구현 및 배경 구현

---
+++ Assets/Scripts/FlappyBirdScripts/Obstacle.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+   public float highPosY
+
+}
---
+++ Assets/Scripts/FlappyBirdScripts/PlayerFlappy.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFlappy : MonoBehaviour
+{
+    Animator animator = null;
+    Rigidbody2D _rigidbody = null;
+
+    [SerializeField] private float flapForce = 6f;
+    public float FlapForce { get => flapForce; }
+
+    [SerializeField] private float forwardSpeed = 3f;
+    public float ForwardSpeed { get => forwardSpeed; }
+
+    [SerializeField] private bool isDead = false;
+    public bool IsDead { get => isDead; }
+    
+    float deathCooldown = 0f;
+    bool isFlap = false;
+
+    public bool godMode = false;
+
+    private void Start()
+    {
+        animator = transform.GetComponentInChildren<Animator>();
+        _rigidbody = transform.GetComponent<Rigidbody2D>();
+
+        if (animator == null)
+            Debug.LogError("Not Founded Animator");
+
+        if (_rigidbody == null)
+            Debug.LogError("Not Founded Rigidbody");
+    }
+
+
+    private void Update()
+    {
+        if (isDead)
+        {
+            if (deathCooldown <= 0)
+            {
+                if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+                {
+                    
+
+                }
+                else deathCooldown -= Time.deltaTime;
+
+            }
+        }
+        else /*isFlap = Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0) ? true : false;     */
+        {
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+            {
+                isFlap = true;
+            }
+        }
+    }
+
+    public void FixedUpdate()
+    {
+        if (isDead)
+            return;
+
+        Vector3 velocity = _rigidbody.velocity;
+
+        velocity.x = forwardSpeed;
+
+        if(isFlap)
+        {
+            velocity.y += flapForce;
+            isFlap = false;
+        }
+
+        _rigidbody.velocity = velocity;
+
+        // 회전 각도 제한
+        float angle = Mathf.Clamp((_rigidbody.velocity.y * 10f), -90, 90);
+
+        // 오브젝트 움직임을 부드럽게 보간하기 위해 lerp를 사용
+        float lerpAngle = Mathf.Lerp(_rigidbody.velocity.y, angle, Time.fixedDeltaTime * 5f);
+
+        transform.rotation = Quaternion.Euler(0, 0, lerpAngle);
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (godMode)
+            return;
+        if (isDead)
+            return;
+
+        animator.SetInteger("IsDie", 1);
+        isDead = true;
+        deathCooldown = 1f;
+    }
+
+}
